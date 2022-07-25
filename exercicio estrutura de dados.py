3235bf26f5a2945cbdb18cb7fdeb10b992bec716
wellerson <100451934+well1999@users.noreply.github.com>
2022-07-25 12:00:39 -0300

    estrutura de dados python

---
+++ exercicio estrutura de dados.py
@@ -0,0 +1,86 @@
+## atividade 1
+movie_list = [ 
+'um sonho de liberdade',
+'o poderoso chefão',
+'batman:os cavaleiros'
+'o poderoso chefão 2'
+'dois homens e uma setença'
+'a lista de shindle'
+'o senhor dos aneis'
+'pulp function'
+'tres homens em conflito'
+'clube da luta']
+print(movie_list)
+print(type(movie_list))
+
+
+## movimentação de ranking
+movie_list = [ 
+'um sonho de liberdade','o poderoso chefão','batman:os cavaleiros','o poderoso chefão 2','dois homens e uma setença','a lista de shindle','o senhor dos aneis',
+'pulp function','tres homens em conflito','clube da luta']
+print(movie_list)
+
+## Atividade 2
+movie_list = [ 
+'um sonho de liberdade',
+'o poderoso chefão 2',
+'batman:os cavaleiros'
+'o poderoso chefão 2'
+'dois homens e uma setença'
+'a lista de shindle'
+'o senhor dos aneis'
+'pulp function'
+'tres homens em conflito'
+'clube da luta']
+movie_list.remove("o poderoso chefão 2")
+print("o poderoso chefão 2")
+
+## atidade 3 
+
+movie_on= ['um sonho de liberdade',
+'o poderoso chefão',
+'batman:os cavaleiros'
+'o poderoso chefão 2'
+'dois homens e uma setença'
+'a lista de shindle'
+'o senhor dos aneis'
+'pulp function'
+'tres homens em conflito'
+'clube da luta']
+movie = {'nome': 'um sonho de liberdade', 'ano': '1994',
+'sinopse': 'Dois_homens presos se reúnem ao longo de vários anos, encontrando consolo e eventual redenção através de atos de decência comum'} 
+movie_on.append(movie_on)
+
+movie = {'nome': 'o poderoso chefão', 'ano': '1972',
+'sinopse': 'O patriarca idoso de uma dinastia do crime organizado transfere o controle de seu império clandestino para seu filho relutante.'} 
+movie_on.append(movie_on)
+
+movie = {'nome': 'batman:os cavaleiros', 'ano': '2008',
+'sinopse': 'Quando a ameaça conhecida como O Coringa surge de seu passado, causa estragos e caos nas pessoas de Gotham. O Cavaleiro das Trevas deve aceitar um dos maiores testes para combater a injustiça'} 
+movie_on.append(movie_on)
+
+movie = {'nome': '12 homens e uma setença', 'ano': '1957',
+'sinopse': 'Um jurado que se aposenta tenta evitar um erro judicial forçando seus colegas a reconsiderarem as evidências.'} 
+movie_on.append(movie_on)
+
+movie = {'nome': 'a lista de shindle', 'ano': '1993',
+'sinopse': 'Depois de testemunhar a perseguição dos judaicos na Polônia ocupada pelos alemães durante a Segunda Guerra Mundial, o industrial Oskar Schindler se começa a preocupar com sua força de trabalho judaica.'} 
+movie_on.append(movie_on)
+
+movie = {'nome': 'o senhor dos aneis', 'ano': '2003',
+'sinopse': 'Gandalf e Aragorn lideram o Mundo dos Homens contra o exército de Sauron para desviar o olhar de Frodo e Sam quando eles se aproximam á Montanha da Perdição com o Um Anel.'} 
+movie_on.append(movie_on)
+
+movie = {'nome': 'pulp function', 'ano': '1994',
+'sinopse': 'As vidas de dois assassinos da máfia, um boxeador, um gângster e sua esposa, e um par de bandidos se entrelaçam em quatro histórias de violência e redenção.'} 
+movie_on.append(movie_on)
+
+movie = {'nome': 'tres homens em conflito', 'ano': '1966',
+'sinopse': 'Um impostor se junta com dois homens para encontrar fortuna num remoto cemitério.'} 
+movie_on.append(movie_on)
+
+movie = {'nome': 'clube da luta', 'ano': '1999',
+'sinopse': 'Um trabalhador de escritório e um fabricante de sabonetes que cuidam do diabo formam um clube de luta clandestino que evolui para algo muito maior.'} 
+movie_on.append(movie_on)
+
+print(movie_on)
